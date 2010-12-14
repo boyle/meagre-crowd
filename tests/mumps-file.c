67c8f0e0746b329ecc099d5b9a40ebd430a61e14
Alistair Boyle <alistair.js.boyle@gmail.com>
2010-12-13 19:17:46 -0500

    Relicensed as LGPL 3.0
     - notice added as header to all files

--- tests/mumps-file.c
+++ tests/mumps-file.c
@@ -1,3 +1,21 @@
+/* Meagre-Crowd: A sparse distributed matrix solver testbench for performance benchmarking.
+ * Copyright (C) 2010 Alistair Boyle <alistair.js.boyle@gmail.com>
+ *
+ *     This file is part of Meagre-Crowd.
+ *
+ *     Meagre-Crowd program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /* Example program using the C interface to the
 * double precision version of MUMPS, dmumps_c.
 * We solve the system A x = RHS with
