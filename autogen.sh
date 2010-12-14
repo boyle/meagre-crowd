293a8ca9f1aac37fd4a46472ab2cf4413936f25c
Alistair Boyle <alistair.js.boyle@gmail.com>
2010-12-13 23:52:52 -0500

    minor: remove generated files from repository - licenses, ChangeLog
      - autogen.sh is updated to generate the ChangeLog from git and
        download the licenses before running autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,29 @@
 #! /bin/sh
 
+# generate ChangeLog from git shortlog
+echo "rebuilding ChangeLog"
+# loop through sequential tags to build ChangeLog
+# 0.0.1..0.0.2
+# 0.0.2..0.0.3
+# etc
+[[ -f ChangeLog ]] && rm ChangeLog
+T=( $(git tag -l) )
+for i in $(seq 1 $((${#T[@]}-1)) | tac); do
+  echo -ne "Release ${T[$i]}:                                          " >> ChangeLog
+  date --date=- +'%b %d, %Y' --date="$(git show -s --format="%ci" ${T[$i]} | tail -1)" >> ChangeLog
+  git shortlog -w79,6,9 ${T[$i-1]}..${T[$i]} | grep -v 'minor:' | grep -e '^ ' >> ChangeLog
+  echo "" >> ChangeLog
+done
+
+if [[ ! -f COPYING ]]; then
+  echo "get GPLv3"
+  wget -q http://www.gnu.org/licenses/gpl-3.0.txt -O COPYING
+fi
+
+if [[ ! -f COPYING.LESSER ]]; then
+  echo "get LGPLv3"
+  wget -q http://www.gnu.org/licenses/lgpl-3.0.txt -O COPYING.LESSER
+fi
+
+echo "update configure.ac, etc"
 autoreconf --install --warnings=all $*
