8b07265c88fea745ac876a1b5e36e19789a2b82f
Alistair Boyle <alistair.js.boyle@gmail.com>
2011-01-13 00:23:53 -0500

    minor: rename data.{c,h} -> matrix.{c,h}

--- src/matrix.c
+++ src/matrix.c
@@ -17,7 +17,7 @@
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "config.h"
-#include "data.h"
+#include "matrix.h"
 
 #include <stdlib.h>
 #include <string.h>
--- src/matrix.h
+++ src/matrix.h
@@ -16,8 +16,8 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef _DATA_H_
-#define _DATA_H_
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
 
 #include "config.h"
 
